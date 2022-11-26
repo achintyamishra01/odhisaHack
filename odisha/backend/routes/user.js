7cf2cb9fec5608e9dca51f8337f8ba0e39beedce
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2022-11-26 23:51:51 +0530

    "Routed Frontend"

--- odisha/backend/routes/user.js
+++ odisha/backend/routes/user.js
@@ -3,36 +3,34 @@ const router = express.Router();
 const twilio = require("twilio");
 const dotenv = require("dotenv");
 const user = require("../schema/userschema");
-const fs=require("fs")
-
-
+const fs = require("fs");
 
 const arr = [
-  {"750017":"Bhubaneshwar Municipal Corporation"},
-    {"751002":"Bhubaneshwar Municipal Corporation"},
-    {"751006":"Bhubaneshwar Municipal Corporation"},
-    {"751003":"Bhubaneshwar Municipal Corporation"},
-    {"751001":"Bhubaneshwar Municipal Corporation"},
-    {"753001":"Cuttack Municipal Corporation"},
-    {"753006":"Cuttack Municipal Corporation"},
-    {"753002":"Cuttack Municipal Corporation"},
-    {"753004":"Cuttack Municipal Corporation"},
-    {"753003":"Cuttack Municipal Corporation"},
-    {"760001":"Berhampur Municipal Corporation"},
-    {"760002":"Berhampur Municipal Corporation"},
-    {"760004":"Berhampur Municipal Corporation"},
-    {"760005":"Berhampur Municipal Corporation"},
-    {"760008":"Berhampur Municipal Corporation"},
-    {"769001":"Rourkela Municipal Corporation"},
-    {"769002":"Rourkela Municipal Corporation"},
-    {"769003":"Rourkela Municipal Corporation"},
-    {"769004":"Rourkela Municipal Corporation"},
-    {"769005":"Rourkela Municipal Corporation"},
-    {"768001":"Sambalpur Municipal Corporation"},
-    {"768002":"Sambalpur Municipal Corporation"},
-    {"768003":"Sambalpur Municipal Corporation"},
-    {"768004":"Sambalpur Municipal Corporation"},
-    {"768006":"Sambalpur Municipal Corporation"}
+  { 750017: "Bhubaneshwar Municipal Corporation" },
+  { 751002: "Bhubaneshwar Municipal Corporation" },
+  { 751006: "Bhubaneshwar Municipal Corporation" },
+  { 751003: "Bhubaneshwar Municipal Corporation" },
+  { 751001: "Bhubaneshwar Municipal Corporation" },
+  { 753001: "Cuttack Municipal Corporation" },
+  { 753006: "Cuttack Municipal Corporation" },
+  { 753002: "Cuttack Municipal Corporation" },
+  { 753004: "Cuttack Municipal Corporation" },
+  { 753003: "Cuttack Municipal Corporation" },
+  { 760001: "Berhampur Municipal Corporation" },
+  { 760002: "Berhampur Municipal Corporation" },
+  { 760004: "Berhampur Municipal Corporation" },
+  { 760005: "Berhampur Municipal Corporation" },
+  { 760008: "Berhampur Municipal Corporation" },
+  { 769001: "Rourkela Municipal Corporation" },
+  { 769002: "Rourkela Municipal Corporation" },
+  { 769003: "Rourkela Municipal Corporation" },
+  { 769004: "Rourkela Municipal Corporation" },
+  { 769005: "Rourkela Municipal Corporation" },
+  { 768001: "Sambalpur Municipal Corporation" },
+  { 768002: "Sambalpur Municipal Corporation" },
+  { 768003: "Sambalpur Municipal Corporation" },
+  { 768004: "Sambalpur Municipal Corporation" },
+  { 768006: "Sambalpur Municipal Corporation" },
 ];
 
 require("dotenv").config();
@@ -40,23 +38,23 @@ require("dotenv").config();
 // const client = new twilio(process.env.ACC_SID, process.env.AUTH_TOKEN); // UNCOMMENT THIS
 
 router.post("/complain", (req, res) => {
-  console.log(req.body.pincode)
+  console.log(req.body.pincode);
   let pincode = parseInt(req.body.pincode);
   let phone = parseInt(req.body.phone);
-  let municipality=req.body.municipality
+  let municipality = req.body.municipality;
   let ticketId = Date.now();
 
   const person = user.create({
     name: req.body.name,
-    pincode: pincode, 
+    pincode: pincode,
     address: req.body.address,
     // e_waste: req.body.e_waste,
     phone: phone,
     // gov_com: req.body.gov_com,
     ticketId: ticketId,
-    municipality:municipality
+    municipality: municipality,
   });
-  // send_SMS(phone,ticketId);  // UNCOMMENT THIS
+  // send_SMS(phone, ticketId); // UNCOMMENT THIS
   res.status(200).json({
     success: true,
     message:
@@ -77,10 +75,9 @@ router.post("/track", async (req, res) => {
     res.status(200).json({ success: true, message: "TicketId doesnot exist" });
   }
 });
-router.post("/municipality",async(req,res)=>{
-    res.status(200).json({success:true,data:arr,message:"data_sent"})
+router.post("/municipality", async (req, res) => {
+  res.status(200).json({ success: true, data: arr, message: "data_sent" });
 });
- 
 
 function send_SMS(num, ticketId) {
   let mes = `Dear user,  Your request with ticket id : ${ticketId} has been generated succesfully and will be resolved within 3 working days`;
--- odisha/src/App.js
+++ odisha/src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import Complain from './components/complain/Complain';
-import Track from "./components/track/track"
+import "./App.css";
+import Complain from "./components/complain/complain";
+import Track from "./components/track/track";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <>
-    <Complain></Complain>
-    <Track></Track>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Complain />} />
+        <Route path="/track" element={<Track />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
