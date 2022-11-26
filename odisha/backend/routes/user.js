ba84ac48ede24076b0a3422486bdc65d6e8bf43b
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2022-11-26 20:50:32 +0530

    "Added SMS FACILITY"

--- odisha/backend/routes/user.js
+++ odisha/backend/routes/user.js
@@ -1,39 +1,62 @@
 const express = require("express");
 const router = express.Router();
-const user = require('../schema/userschema')
+const twilio = require("twilio");
+const dotenv = require("dotenv");
+const user = require("../schema/userschema");
 // const bcrypt = require("bcrypt");
 
-router.post('/complain', (req, res) => {
+require("dotenv").config();
 
-    let pincode = parseInt(req.body.pincode);
-    let phone = parseInt(req.body.phone);
-    let ticketId = Date.now();
+// const client = new twilio(process.env.ACC_SID, process.env.AUTH_TOKEN); // UNCOMMENT THIS
 
-    const person = user.create({
-        name: req.body.name,
-        pincode: pincode,
-        address: req.body.address,
-        e_waste: req.body.e_waste,
-        phone: phone,
-        gov_com: req.body.gov_com,
-        ticketId: ticketId
+router.post("/complain", (req, res) => {
+  let pincode = parseInt(req.body.pincode);
+  let phone = parseInt(req.body.phone);
+  let ticketId = Date.now();
+
+  const person = user.create({
+    name: req.body.name,
+    pincode: pincode,
+    address: req.body.address,
+    e_waste: req.body.e_waste,
+    phone: phone,
+    gov_com: req.body.gov_com,
+    ticketId: ticketId,
+  });
+  //   send_SMS(phone);  // UNCOMMENT THIS
+
+  res
+    .status(200)
+    .json({
+      success: true,
+      message:
+        "Ticket Generated successfully, and has been sent as an SMS to the phone number provided",
     });
-
-    res.status(200).json({ success: true, message: "User registered successfully" });
-
-})
-
-router.post('/track', async (req, res) => {
-    const ticketId = req.body.ticketId;
-
-    const getData = await user.findOne({ ticketId: ticketId });
-
-    if (getData) {
-        res.status(200).json({ success: true, data: getData, message: "TicketId found" });
-    }
-    else {
-        res.status(200).json({ success: true, message: "TicketId doesnot exist" });
-    }
 });
+
+router.post("/track", async (req, res) => {
+  const ticketId = req.body.ticketId;
+
+  const getData = await user.findOne({ ticketId: ticketId });
+
+  if (getData) {
+    res
+      .status(200)
+      .json({ success: true, data: getData, message: "TicketId found" });
+  } else {
+    res.status(200).json({ success: true, message: "TicketId doesnot exist" });
+  }
+});
+
+function send_SMS(num) {
+  client.messages
+    .create({
+      body: "Hello from Node",
+      to: "+91" + num, // Text this number
+      from: "+15139604746", // From Twilio number
+    })
+    .then((message) => console.log(message.sid))
+    .catch((error) => console.log(error));
+}
 
 module.exports = router;
