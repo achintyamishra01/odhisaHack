9bf6714e0701296ffd927cbd1ce5a06d64ce8f50
achintyamishra01 <achintyamishra01@gmail.com>
2022-11-27 17:07:29 +0530

    changes

--- odisha/src/App.js
+++ odisha/src/App.js
@@ -3,6 +3,7 @@ import Complain from "./components/complain/Complain";
 import Track from "./components/track/Track";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Committee from "./components/committee/Committee";
+import Municipal from "./components/municipal/Municipal";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Complain />} />
         <Route path="/committee" element={<Committee />} />
+        <Route path="/municipal" element={<Municipal />} />
         <Route path="/track" element={<Track />} />
       </Routes>
     </BrowserRouter>
