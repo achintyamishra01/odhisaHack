43f273f650052250d2f606e0a520f0881856102c
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2022-11-27 20:07:29 +0530

    Committeee dashboard

--- odisha/src/components/committee/CommitteeDashboard.js
+++ odisha/src/components/committee/CommitteeDashboard.js
@@ -1,51 +1,57 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../complain/complain.css";
 
 const CommitteelDashboard = () => {
-    const [data1, setdata1] = useState([])
-    const navigate = useNavigate();
-    useEffect(() => {
+  const [data1, setdata1] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
     //   if(!localStorage.getItem("municipal")){
     //     navigate("/municipal")
     //   }
-      fetchComplaints()
-   
-    }, [])
-    
-    const fetchComplaints=async()=>{
-      
-        const res = await fetch("/api/fetchGovComplaints", {
-            method: "POST",
-            headers: {
-              "content-type": "application/json",
-            }
-           
-          });
-          const c=await res.json();
-          
-          if(c.data){
-            console.log(c.data)
-            setdata1(c.data);
-            
-          }
-          else{
-            document.getElementById("Gov_complaints").innerHTML="No complaints so far!!!"
-          }
-        
-        
-    }
-    const renderList = data1.map((item, index) => (
-        
-        <p>
-            {item.name}  {item.address}
-            
-        </p>
-      ));
-  return (
-    <>
-    <div id='Gov_complaints'>{renderList}</div>
-    </>
-  )
-}
+    fetchComplaints();
+  }, []);
 
-export default CommitteelDashboard
+  const fetchComplaints = async () => {
+    const res = await fetch("/api/fetchGovComplaints", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const c = await res.json();
+
+    if (c.data) {
+      console.log(c.data);
+      setdata1(c.data);
+    } else {
+      document.getElementById("Gov_complaints").innerHTML =
+        "No complaints so far!!!";
+    }
+  };
+  const renderList = data1.map((item, index) => (
+    <p>
+      {item.name} {item.address}
+    </p>
+  ));
+  return (
+    <div>
+      <div id="gchead">
+        <a href="/">
+          <img src={require("../../Assets/logo.png")} alt="" />
+        </a>
+        <div>
+          <h2>Governing Committee Dashboard</h2>
+        </div>
+      </div>
+      <div id="outerdashcom">
+        <h2>
+          <u>Industry Complaint :</u>
+        </h2>
+        <table id="Gov_complaints">{renderList}</table>
+      </div>
+    </div>
+  );
+};
+
+export default CommitteelDashboard;
