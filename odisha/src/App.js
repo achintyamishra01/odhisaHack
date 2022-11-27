3e2595d3de47264fe27e6fcdee4d18c7b39bb6e0
achintyamishra01 <achintyamishra01@gmail.com>
2022-11-27 19:38:39 +0530

    modified:   odisha/src/App.js

--- odisha/src/App.js
+++ odisha/src/App.js
@@ -4,13 +4,9 @@ import Track from "./components/track/Track";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Committee from "./components/committee/Committee";
 import Municipal from "./components/municipal/Municipal";
-<<<<<<< HEAD
-import Industry from "./components/industry/Industry";
-=======
 import MunicipalDashboard from "./components/municipal/MunicipalDashboard";
 import CommitteelDashboard from "./components/committee/CommitteeDashboard";
 
->>>>>>> 1f845eb1d8b5048a5ce848477c701b22372bd116
 
 function App() {
   return (
@@ -20,12 +16,8 @@ function App() {
         <Route path="/committee" element={<Committee />} />
         <Route path="/municipal" element={<Municipal />} />
         <Route path="/track" element={<Track />} />
-<<<<<<< HEAD
-        <Route path="/industry" element={<Industry/>} />
-=======
         <Route path="/municipalDashboard" element={<MunicipalDashboard />} />
         <Route path="/committeeDashboard" element={<CommitteelDashboard />} />
->>>>>>> 1f845eb1d8b5048a5ce848477c701b22372bd116
       </Routes>
     </BrowserRouter>
   );
