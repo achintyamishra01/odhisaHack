9f177d22869f2b44618826aa90fae77309c6c48e
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2022-11-29 20:49:38 +0530

    Improved the logout button style

--- odisha/src/components/municipal/MunicipalDashboard.js
+++ odisha/src/components/municipal/MunicipalDashboard.js
@@ -25,43 +25,40 @@ const MunicipalDashboard = () => {
     const c = await res.json();
 
     if (c.success) {
-      if(c.data){
-        
+      if (c.data) {
         setdata1(c.data);
       }
-      
+
       // console.log(c.data);
     } else {
       alert("something went wrong");
     }
   };
- 
-  async function resolveComplaints(ticketId){
-    console.log(ticketId)
-    let tId={ticketId}
+
+  async function resolveComplaints(ticketId) {
+    console.log(ticketId);
+    let tId = { ticketId };
     const res = await fetch("/api/resolveMunicipalComplaints", {
       method: "POST",
       headers: {
         "content-type": "application/json",
       },
-      body:JSON.stringify(tId)
+      body: JSON.stringify(tId),
     });
-    const d=await res.json();
-    console.log(d)
-    if(d.success){
-      console.log("reloading")
+    const d = await res.json();
+    console.log(d);
+    if (d.success) {
+      console.log("reloading");
       window.location.reload();
+    } else {
+      alert("Something went wrong");
     }
-    else{
-      alert("Something went wrong")
-    }
+  }
+  const logout = async () => {
+    localStorage.removeItem("municipal");
+    window.location.reload();
+  };
 
-  }
-  const logout=async()=>{
-    localStorage.removeItem("municipal")
-    window.location.reload()
-  }
-  
   const renderList = data1.map((item, index) => (
     <tr>
       <td className="trackd">{item.ticketId}</td>
@@ -72,7 +69,12 @@ const MunicipalDashboard = () => {
       <td className="trackd">{item.phone}</td>
       <td className="trackd">{item.status}</td>
       <td className="trackd">
-        <button className="resbut" onClick={()=>resolveComplaints(item.ticketId)}>Yes?</button>
+        <button
+          className="resbut"
+          onClick={() => resolveComplaints(item.ticketId)}
+        >
+          Yes?
+        </button>
       </td>
     </tr>
   ));
@@ -82,33 +84,37 @@ const MunicipalDashboard = () => {
         <a href="/">
           <img src={require("../../Assets/logo.png")} alt="" />
         </a>
-        <div>
+        <div id="muparlog">
           <h2>Municipal Dashboard</h2>
-          <button onClick={logout}>Logout</button>
+          <button onClick={logout} id="mlout">
+            Logout
+          </button>
         </div>
       </div>
       <div id="outerdashmun">
         <h2>
           <u>Civilian Requests :</u>
         </h2>
-        {data1.length!==0 && <div id="complaints">
-          <table class="tab">
-            <tr>
-              <th className="trackh">TicketID</th>
-              <th className="trackh">Municipality</th>
-              <th className="trackh">Compalainee</th>
-              <th className="trackh">Address</th>
-              <th className="trackh">Pincode</th>
-              <th className="trackh">Phone</th>
-              <th className="trackh">Status</th>
-              <th className="trackh">resolved</th>
-            </tr>
-            {renderList}
-          </table>
-        </div>}
-        {
-          data1.length===0 && <div id ="complaints1">No complaints so far!!</div>
-        }
+        {data1.length !== 0 && (
+          <div id="complaints">
+            <table class="tab">
+              <tr>
+                <th className="trackh">TicketID</th>
+                <th className="trackh">Municipality</th>
+                <th className="trackh">Compalainee</th>
+                <th className="trackh">Address</th>
+                <th className="trackh">Pincode</th>
+                <th className="trackh">Phone</th>
+                <th className="trackh">Status</th>
+                <th className="trackh">resolved</th>
+              </tr>
+              {renderList}
+            </table>
+          </div>
+        )}
+        {data1.length === 0 && (
+          <div id="complaints1">No complaints so far!!</div>
+        )}
       </div>
     </div>
   );
